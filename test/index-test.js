4697b175cdec2b2db8f6e9f6dc4504c17f76a5bf
Jerkbert <raballantyne@gmail.com>
2020-03-17 16:08:10 -0600

    confirming testing works

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 const index = require('../index');
 
 describe('Index', function() {
-  it('index should return hello', function*() {
+  it('index should return hello', function() {
     assert.equal(index(), 'hello');
   });
 });
