46d68c42fd41ae83525acbd362d2bbcd2124bc31
Jerkbert <raballantyne@gmail.com>
2020-03-18 11:43:14 -0600

    solution added

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports = function() {
-  return 'hello';
+const sumString = str => {
+  return str
+    .split('')
+    .reduce((a, b) => {
+      return +a + +b;
+    })
+    .toString();
 };
+
+const sumArray = array => {
+  return array.reduce((a, b) => {
+    return a + b;
+  });
+};
+
+const multiplyString = str => {
+  return str.split('').reduce((a, b) => {
+    return a * b;
+  });
+};
+
+//don't code below this line...
+module.exports.sumString = sumString;
+module.exports.multiplyString = multiplyString;
+module.exports.sumArray = sumArray;
--- test/index-test.js
+++ test/index-test.js
@@ -1,8 +1,26 @@
 const assert = require('chai').assert;
 const index = require('../index');
 
-describe('Index', function() {
-  it('index should return hello', function() {
-    assert.equal(index(), 'hello');
+describe('#codeTests', function() {
+  it('sumString returns the correct sum of a string of numbers', function() {
+    let numbers = '02415';
+    assert.equal(index.sumString(numbers), '12');
+  });
+
+  it('sumString should return a string', function() {
+    let numbers = '123';
+    let result = index.sumString(numbers);
+    assert.typeOf(result, 'string');
+  });
+
+  it('sumArray should return the correct sum of elements in an array', function() {
+    let numbers = [1, 2, 3];
+    let result = index.sumArray(numbers);
+    assert.equal(result, 6);
+  });
+
+  it('multiplyString returns the correct product of a string of numbers', function() {
+    let numbers = '124';
+    assert.equal(index.multiplyString(numbers), 8);
   });
 });
